3a78664b43a15aba75a7b9375dae69e624d7fbb1
Esteban, Prince <qpwgesteban@tip.edu.ph>
2024-10-21 09:02:34 +0800

    Add files via upload

---
+++ Lab5/gui_buttonclicked.py
@@ -0,0 +1,42 @@
+  import sys
+  from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QMessageBox
+  from PyQt5.QtGui import QIcon
+  from PyQt5.QtCore import pyqtSlot
+
+  class App(QWidget):
+
+    def __init__(self):
+      super().__init__()
+      self.title = 'PyQt5 Button'
+      self.x = 200
+      self.y = 200
+      self.width = 300
+      self.height = 300
+      self.initUI()
+
+    def initUI(self):
+      self.setWindowTitle(self.title)
+      self.setGeometry(self.x, self.y, self.width, self.height)
+      self.setWindowIcon(QIcon('pythonico'))
+
+      self.button = QPushButton('Click me!', self)
+      self.button.setToolTip('You hovered over me')
+      self.button.move(100, 100)
+      self.button.clicked.connect(self.on_click)
+
+      self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+      buttonReply = QMessageBox.question(self, 'Testing Response', 'Do you like python QT?', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+
+      if buttonReply == QMessageBox.Yes:
+        QMessageBox.warning(self, 'Evaluation', 'User clicked Yes', QMessageBox.Ok, QMessageBox.Ok)
+
+      else:
+        QMessageBox.information(self, 'Evaluation', 'User clicked Yes', QMessageBox.Ok, QMessageBox.Ok)
+      
+  if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
